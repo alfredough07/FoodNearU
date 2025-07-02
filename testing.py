58293b1eaf3664b0e5877fe278849dbda177175a
Alfredo Zuniga <diaz.alfredo2001@gmail.com>
2025-07-02 15:04:14 -0500

    PEP8

--- testing.py
+++ testing.py
@@ -8,20 +8,23 @@ gmaps = googlemaps.Client(key=API_KEY)
 
 
 # Get inputs
-geocode = gmaps.geocode(input("Please enter your current location (street address, cite, state): ")) # 411 N State St, Ann Arbor, MI
+geocode = gmaps.geocode(
+    input("Please enter your current location (street address, cite, state): ")
+)  # 411 N State St, Ann Arbor, MI
 rad = float(input("Please enter the radius to search in miles: ")) * 1609.34
 
 # Search for places near a location
 # 'Restaurnt' is too selective as a type
 
 
-places = gmaps.places(location=geocode[0]['geometry']['location'], radius=rad, type='restaurant')
-results = places['results']
-while 'next_page_token' in places:
+places = gmaps.places(
+    location=geocode[0]["geometry"]["location"], radius=rad, type="restaurant"
+)
+results = places["results"]
+while "next_page_token" in places:
     time.sleep(2)
-    places = gmaps.places(page_token=places['next_page_token'])
-    results.extend(places['results'])
-
+    places = gmaps.places(page_token=places["next_page_token"])
+    results.extend(places["results"])
 
 
 for result in results:
